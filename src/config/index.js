e23196f4b553c1ba71eab51941b6f98cdc58654b
dviiiii <913284195@qq.com>
2018-12-14 10:00:13 +0800

    修改baseURL

--- src/config/index.js
+++ src/config/index.js
@@ -13,8 +13,8 @@ export default {
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: '/api',
-    pro: '/api'
+    dev: '/hqdba',
+    pro: '/hqdba'
   },
   /**
    * @description 默认打开的首页的路由name值，默认为home
